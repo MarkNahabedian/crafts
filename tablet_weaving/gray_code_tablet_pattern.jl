afbf785d0a0274febb5181b0edc8c75dcb80c8c5
MarkNahabedian <naha@mit.edu>
2022-08-11 09:03:51 -0400

    rotate! and shot!

--- tablet_weaving/gray_code_tablet_pattern.jl
+++ tablet_weaving/gray_code_tablet_pattern.jl
@@ -40,17 +40,97 @@ end
 	c::T
 	d::T
 	sz = :s
-	rotation::Int = 0
-	# we need to know whether a thread was rotated forwards or backwards to know the direction of the stitch.
+	accumulated_rotation::Int = 0
+	this_shot_rotation = 0
 end
 
+# ╔═╡ 86033a92-cd04-4c52-845d-89a8a473506c
+abstract type RotationDirection end
+
+# ╔═╡ bb8a5f20-62af-4f28-b0df-85af57beb8f3
+begin
+	struct Clockwise <: RotationDirection end
+	struct CounterClockwise <: RotationDirection end
+	struct Forward <: RotationDirection end
+	struct Backward <: RotationDirection end
+end
+
+# ╔═╡ 50e521b5-c4f7-464d-b6dd-5c7f9d5b4bd0
+"""
+    rotate!(::Tablet, ::AbstractDirection)
+Rotatethe tablet by one position in the specified direction.
+"""
+function rotate! end
+
+# ╔═╡ 9d85d3ef-847b-405c-817b-71097b56fee5
+function rotate!(t::Tablet, ::Clockwise)
+	new_rotation = t.this_shot_rotation + 1
+	@assert abs(t.this_shot_rotation) <= 1
+	t.this_shot_rotation = new_rotation
+	return t
+end
+
+# ╔═╡ 748199f2-e5d8-4272-9120-f8b50264b5d6
+function rotate!(t::Tablet, ::CounterClockwise)
+	new_rotation = t.this_shot_rotation - 1
+	@assert abs(t.this_shot_rotation) <= 1
+	t.this_shot_rotation = new_rotation
+	return t
+end
+
+# ╔═╡ 30c08bee-e3f9-4672-a4d6-29df3ba8a6e5
+rotate!(t::Tablet, ::Forward) =
+	rotate!(t,
+		if t.sz == :s
+			Clockwise()
+		else
+			CounterClockwise()
+		end
+	)
+
+# ╔═╡ 6d796003-f336-44ed-8831-8ea2b56fe865
+rotate!(t::Tablet, ::Backward) =
+	rotate!(t,
+		if t.sz == :z
+			Clockwise()
+		else
+			CounterClockwise()
+		end
+	)
+
 # ╔═╡ 9b217c54-ad77-4ce3-9715-cde19bed7bc4
+"""
+    threads0(::Tablet)
+return the threads of the unrotated Tablet in order.
+"""
 threads0(t::Tablet) = [t.a, t.b, t.c, t.d]
 
 # ╔═╡ 10388ccf-6c52-4113-b34d-e5a54ebec2a7
+"""
+    threads(::Tablet)
+Return the threads of the tabloet as rotated
+"""
 function threads(t::Tablet)
 	ts = threads0(t)
-	[ ts[1 + mod(i - t.rotation, 4)] for i in 0:3 ] 
+	[ ts[1 + mod(i - t.accumulated_rotation, 4)] for i in 0:3 ] 
+end
+
+# ╔═╡ ea0b660e-9512-4ad1-b99a-e17753f47d74
+"""
+    shot!(::Tablet)
+Applies the current rotation to the tablet and returns the colors of the warp threads passing over the top and bottom of the fabric, and the crossing direction (as a forward or backslash character) when looking at that face of the fabric.
+"""
+function shot!(t::Tablet)
+	@assert abs(t.this_shot_rotation) == 1
+	t.accumulated_rotation += t.this_shot_rotation
+	threads = threads(t)
+	front, back, slant = if t.this_shot_rotation > 0
+		threads[1], threads[3], '/'
+	else
+		threads[2], threads[4], '\\'
+	end
+	t.this_shot_rotation = 0
+	return front, back, slant
 end
 
 # ╔═╡ 776e4a65-62f7-4201-b8e5-6d5326e653fa
@@ -59,9 +139,10 @@ For a given `Tablet`, if `a` and `b` are one color and `c` and `d` are another, 
 """
 
 # ╔═╡ 98bb29dc-55e7-4f42-8456-d72079801a3a
-tablets = [Tablet(id = i, a=COLORS[3], b=COLORS[3], c=COLORS[2], d=COLORS[2],
-	rotation = i - 1)
-	for i in 1:8]
+tablets = [Tablet(; id = i, a=COLORS[3], b=COLORS[3], c=COLORS[2], d=COLORS[2],
+	accumulated_rotation = i - 1)
+	for i in 1:8
+]
 
 # ╔═╡ 3c43dd0f-d8d4-460b-a8da-64b3831f6873
 map(threads, tablets)
@@ -155,8 +236,16 @@ uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
 # ╠═b04d2b69-aa8a-4174-8ef8-3e6b797354e7
 # ╠═c2b1f51e-77fb-4e23-94cc-699c124b81c3
 # ╠═0fea18b7-b40e-4ca5-95e5-744e619ea14a
-# ╠═9b217c54-ad77-4ce3-9715-cde19bed7bc4
-# ╠═10388ccf-6c52-4113-b34d-e5a54ebec2a7
+# ╠═86033a92-cd04-4c52-845d-89a8a473506c
+# ╠═bb8a5f20-62af-4f28-b0df-85af57beb8f3
+# ╟─50e521b5-c4f7-464d-b6dd-5c7f9d5b4bd0
+# ╟─9d85d3ef-847b-405c-817b-71097b56fee5
+# ╟─748199f2-e5d8-4272-9120-f8b50264b5d6
+# ╟─30c08bee-e3f9-4672-a4d6-29df3ba8a6e5
+# ╟─6d796003-f336-44ed-8831-8ea2b56fe865
+# ╟─9b217c54-ad77-4ce3-9715-cde19bed7bc4
+# ╟─10388ccf-6c52-4113-b34d-e5a54ebec2a7
+# ╟─ea0b660e-9512-4ad1-b99a-e17753f47d74
 # ╟─776e4a65-62f7-4201-b8e5-6d5326e653fa
 # ╠═98bb29dc-55e7-4f42-8456-d72079801a3a
 # ╠═3c43dd0f-d8d4-460b-a8da-64b3831f6873
