0713ad59bac2d1d6a02abe997047ffd1a83b12ae
MarkNahabedian <naha@mit.edu>
2026-06-07 07:00:06 -0400

    Simple row layout with padding.  All pages currently fit in one row.

--- sewing_stitches/layout_tiff_to_fabric.jl
+++ sewing_stitches/layout_tiff_to_fabric.jl
@@ -17,14 +17,31 @@ function layout_tiff_images(famric_width)
     tiff_width = image_width(images[1])
     tiff_height = image_height(images[1])
     across = floor(famric_width / tiff_width)
+    fabric_width_pixels = famric_width * X_resolution
+    surplus_width = fabric_width_pixels - across * tiff_width * X_resolution
     # Assumes all images are the same size:
     rowcount = ceil(count / across)
     leftover_width = famric_width - across * tiff_width
+    padding_width_pixels = convert(Int, surplus_width / (across + 1))
+    padding_height_pixels = convert(Int, tiff_height * Y_resolution)
     @info("Fabric grid layouut",
-    count, X_resolution, Y_resolution,
-    tiff_width, tiff_height,
-    across, leftover_width, rowcount)
-    # save("stitch_pages.tiff", p)
+          count, X_resolution, Y_resolution,
+          tiff_width, tiff_height,
+          across, leftover_width, surplus_width,
+          padding_width_pixels, padding_height_pixels,
+          rowcount)
+    # Tile the images: Do we need to replace the white pixels with
+    # transparent ones?  Where did the white pixels come from anyway?
+    background_color = RGB{N0f8}(1.0, 1.0, 1.0)
+    horizontal_padding_array = fill(background_color,
+                                    padding_height_pixels,
+                                    padding_width_pixels)
+    image_row_vector = [horizontal_padding_array]
+    for image in images
+        push!(image_row_vector, image)
+        push!(image_row_vector, horizontal_padding_array)
+    end
+    save("stitch_pages.tiff",
+         hcat(image_row_vector...))
 end
-
 
